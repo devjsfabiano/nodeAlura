c82bbe499f30bb7bb822b5e5ca33065b8723e69a
Fabiano Garcia <devjs.fabiano@gmail.com>
2022-08-10 16:39:05 -0300

    first commit

---
+++ src/config/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://alura:fmtSCNoQYftNx5us@alura.yea55fb.mongodb.net/alura-node");
+
+let db = mongoose.connection;
+
+export default db;
+
+                    
